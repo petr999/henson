d85aef2edfacf332bb397c0f28cf3d0efc488d8f
Peter Vereshagin <peter@vereshagin.org>
2021-04-10 17:44:26 +0400

    Revert "feat(prettier): html & js"
    This reverts commit a3f1bfab5147429e303996f1d4ed8715b6f4a295.

--- www/js/henson.js
+++ www/js/henson.js
@@ -1,79 +1,79 @@
-const convertFormToJSON = (form) => {
+const convertFormToJSON = form => {
   let values = {}
-  $(form)
-    .find(":input")
-    .each((i, inputElem) => {
-      let [name, value] = [inputElem.name, inputElem.value]
-      const inputType = inputElem.type
-      if (`checkbox` == inputType) {
-        value = inputElem.checked
-      }
-      const valueInt = parseInt(value)
-      if (valueInt == value) {
-        value = valueInt
-      }
-      values[name] = value
-    })
+  $(form).find(  ":input" ).each( ( i, inputElem ) => {
+    let [ name, value ] = [ inputElem.name, inputElem.value ]
+    const inputType = inputElem.type
+    if( `checkbox` == inputType ){
+      value = inputElem.checked
+    }
+    const valueInt = parseInt( value )
+    if( valueInt == value ){
+      value = valueInt
+    }
+    values[ name ] = value
+  } )
 
   return values
 }
 
 const reloadTable = () => {
-  $("#tasks").DataTable().rows().invalidate("data").draw(false)
+  $('#tasks').DataTable()
+     .rows().invalidate('data')
+     .draw(false)
 }
 
-const onAddNewTaskDone = (message) => {
+const onAddNewTaskDone = message => {
+
   const id = message.id
-  alert(`Task created: ${id}`)
+  alert( `Task created: ${id}` )
 
   reloadTable()
 
-  $("form#add-new-task :input").each((i, inputElem) => {
+  $( "form#add-new-task :input" ).each( ( i, inputElem ) => {
     inputElem.value = ``
-  })
+  } )
+
 }
 
 const onUpdateTaskDone = () => {
-  $("div").modal("hide")
+
+  $('div').modal('hide')
   reloadTable()
+
 }
 
-const onUpdateTaskFail = (httpStatus, errMsg) => {
-  alert("Failed to update task:\n" + errMsg)
-  if (403 == httpStatus) {
-    $("div").modal("hide")
-    $("#auth-modal").modal("show")
+const onUpdateTaskFail = ( httpStatus, errMsg ) => {
+  alert( "Failed to update task:\n" + errMsg )
+  if( 403 == httpStatus ){
+    $('div').modal('hide')
+    $('#auth-modal').modal('show')
   }
 }
-const renderTheWasEdited = (data, type, row, meta) => {
+const renderTheWasEdited = ( data, type, row, meta ) => {
   rv = ``
 
-  if (1 == data) {
-    rv = `Edited by Admin`
-  }
+  if( 1 == data ){ rv = `Edited by Admin` }
 
   return rv
 }
 
-const renderTheIsDone = (data, type, row, meta) => {
+const renderTheIsDone = ( data, type, row, meta ) => {
   rv = ``
 
-  if (1 == data) {
-    rv = `Done`
-  }
+  if( 1 == data ){ rv = `Done` }
 
   return rv
 }
 
-const getEditTaskForm = (row) => {
+const getEditTaskForm = row => {
   let rv = ``
 
-  const [id, taskText, isDone] = [`id`, `taskText`, `isDone`].map((elem) => {
-    val = row[elem]
+  const [ id, taskText, isDone ] = [ `id`, `taskText`, `isDone` ].map( elem => {
+    val = row[ elem ]
     return val
   })
 
-  const checkedAttr = 1 == isDone ? ` checked ` : ` `
+  const checkedAttr = ( 1 == isDone ) ? ` checked ` : ` `
 
   rv = `
   <form id="update-task-${id}" class="update-task">
@@ -98,9 +98,9 @@ const getEditTaskForm = (row) => {
   return rv
 }
 
-const editTaskModal = (row) => {
+const editTaskModal = row => {
   const id = row.id
-  const editTaskForm = getEditTaskForm(row)
+  const editTaskForm = getEditTaskForm( row )
   const rv = `
   <div class="modal fade" id="updateTask-${id}" tabindex="-1" role="dialog" aria-labelledby="updateTaskModalCenterTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
@@ -122,155 +122,149 @@ const editTaskModal = (row) => {
   return rv
 }
 
-const editTaskButton = (data, type, row, meta) => {
+const editTaskButton = ( data, type, row, meta ) => {
   let rv = `<button type="button" class="btn btn-primary"
     data-toggle="modal" data-target="#updateTask-${data}">
   Edit
   </button>`
-  rv += editTaskModal(row)
+  rv += editTaskModal( row )
 
   return rv
 }
 
-const getErrMsgByResponse = (responseJSON) => {
-  const errMsgs = JSON.parse(responseJSON.description)
-  const errMsg = Array.isArray(errMsgs) ? errMsgs.join("\n") : errMsgs
+const getErrMsgByResponse = responseJSON => {
+  const errMsgs = JSON.parse( responseJSON.description )
+  const errMsg = Array.isArray( errMsgs ) ? errMsgs.join( "\n" ) : errMsgs
 
   return errMsg
 }
 
-$(document).ready(() => {
-  $("#tasks").DataTable({
-    searching: false,
-    lengthChange: false,
-    processing: true,
-    serverSide: true,
-    pageLength: 3,
-    order: [[0, "desc"]], // first field is 'id'
-    ajax: {
-      url: "/api/",
-      dataSrc: "data",
-      dataFilter: (response) => {
-        const json = jQuery.parseJSON(response)
+$(document).ready( () => {
+  $('#tasks').DataTable({
+    "searching": false,
+    "lengthChange": false,
+    "processing": true,
+    "serverSide": true,
+    "pageLength": 3,
+    "order": [ [ 0, "desc", ],], // first field is 'id'
+    "ajax": { "url": "/api/",
+      "dataSrc": "data",
+      "dataFilter": response => {
+        const json = jQuery.parseJSON( response )
         const message = json.message
         message.recordsFiltered = message.recordsTotal
         const rv = message
 
-        return JSON.stringify(rv)
+        return JSON.stringify( rv )
       },
     },
-    columns: [
-      ...[`id`, `name`, `email`, `taskText`].map((elem) => {
-        const rv = { data: elem }
+    "columns": [ ... [ `id`, `name`, `email`, `taskText`, ].map( elem => {
+        const rv = { data: elem, }
         return rv
-      }),
-      { data: `isDone`, render: renderTheIsDone },
-      { data: `wasEdited`, render: renderTheWasEdited },
-      { data: "id", render: editTaskButton },
+      }  ),
+        { 'data': `isDone`, 'render': renderTheIsDone, },
+        { 'data': `wasEdited`, 'render': renderTheWasEdited, },
+        { 'data': 'id', 'render': editTaskButton, },
     ],
-    fnDrawCallback: function (oSettings) {
-      $(".update-task").bind("submit", function (event) {
-        event.preventDefault()
+    "fnDrawCallback": function( oSettings ) {
+      $('.update-task').bind('submit', function( event ){
+          event.preventDefault()
 
-        const form = this
-        const formValues = convertFormToJSON(form)
+          const form = this
+          const formValues = convertFormToJSON( form )
 
-        $.ajax({
-          type: "PUT",
-          url: "/api/",
-          data: JSON.stringify(formValues),
-          contentType: "application/json",
-          dataType: "json",
-        })
-          .done(function () {
+          $.ajax({
+              type: "PUT",
+              url: "/api/",
+              data: JSON.stringify( formValues ),
+              contentType: "application/json",
+              dataType: "json"
+          }).done(function() {
+
             onUpdateTaskDone()
-          })
-          .fail(function (jqXHR) {
+
+          }).fail(function(jqXHR) {
             const httpStatus = jqXHR.status
             const responseJSON = jqXHR.responseJSON
             const errMsg = responseJSON.description
-            onUpdateTaskFail(httpStatus, errMsg)
-            return true
+            onUpdateTaskFail( httpStatus, errMsg )
+            return true;
           })
 
-        return true
+          return true
       })
     },
   })
 
-  $("form#add-new-task").bind("submit", function (event) {
-    event.preventDefault()
 
-    const form = this
-    const formValues = convertFormToJSON(form)
+  $('form#add-new-task').bind('submit', function( event ){
+      event.preventDefault()
 
-    $.ajax({
-      type: "POST",
-      url: "/api/",
-      data: JSON.stringify(formValues),
-      contentType: "application/json",
-      dataType: "json",
-    })
-      .done(function (jqXHR) {
+      const form = this
+      const formValues = convertFormToJSON( form )
+
+      $.ajax({
+          type: "POST",
+          url: "/api/",
+          data: JSON.stringify( formValues ),
+          contentType: "application/json",
+          dataType: "json"
+      }).done(function(jqXHR ) {
         const message = jqXHR.message
 
-        onAddNewTaskDone(message)
-      })
-      .fail(function (jqXHR) {
-        const responseJSON = jqXHR.responseJSON
-        const errMsg = getErrMsgByResponse(responseJSON)
-        alert("Failed to add task: \n" + errMsg)
+        onAddNewTaskDone( message )
+
+      }).fail(function( jqXHR ) {
+          const responseJSON = jqXHR.responseJSON
+          const errMsg = getErrMsgByResponse( responseJSON )
+          alert("Failed to add task: \n" + errMsg )
       })
 
-    return true
+      return true
   })
 
-  $("form#auth-login").bind("submit", function (event) {
-    event.preventDefault()
+  $('form#auth-login').bind('submit', function( event ){
+      event.preventDefault()
 
-    const form = this
-    const formValues = convertFormToJSON(form)
+      const form = this
+      const formValues = convertFormToJSON( form )
 
-    $.ajax({
-      type: "POST",
-      url: "/api/user/login",
-      data: JSON.stringify(formValues),
-      contentType: "application/json",
-      dataType: "json",
-    })
-      .done(function () {
-        alert(`Login: done.`)
-        $(form)
-          .find(":input")
-          .each((i, inputElem) => {
-            inputElem.value = ``
-          })
-        $("div").modal("hide")
-      })
-      .fail(function () {
+      $.ajax({
+          type: "POST",
+          url: "/api/user/login",
+          data: JSON.stringify( formValues ),
+          contentType: "application/json",
+          dataType: "json"
+      }).done(function() {
+        alert( `Login: done.` )
+        $(form).find( ":input" ).each( ( i, inputElem ) => {
+          inputElem.value = ``
+        } )
+        $('div').modal('hide')
+      }).fail(function() {
         alert("Failed to log in! Check name and/or passwd")
       })
 
-    return true
+      return true
   })
 
-  $("#auth-logout").on("click", function (event) {
-    event.preventDefault()
+  $('#auth-logout').on('click', function(event) {
+      event.preventDefault();
 
-    $.ajax({
-      type: "POST",
-      url: "/api/user/logout",
-      data: ``,
-      contentType: "application/json",
-      dataType: "json",
-    })
-      .done(function () {
-        alert(`Logout: done.`)
-      })
-      .fail(function () {
+      $.ajax({
+          type: "POST",
+          url: "/api/user/logout",
+          data: ``,
+          contentType: "application/json",
+          dataType: "json"
+      }).done(function() {
+        alert( `Logout: done.` )
+      }).fail(function() {
         alert("Failed to log out!")
       })
 
-    return true
-  })
-})
+      return true
+  });
+
+} )
+
