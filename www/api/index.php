e7ed02fe93cc3262f815018bcadb64b449a14792
Peter Vereshagin <peter@vereshagin.org>
2021-04-10 19:01:30 +0400

    style(php): psr2

--- www/api/index.php
+++ www/api/index.php
@@ -1,21 +1,22 @@
 <?php
 
-function getLoaderPath(){
-  $scriptName = $_SERVER['SCRIPT_NAME'];
-  $path = dirname( $scriptName );
-  $pathArr = array_filter( explode('/', $path), function( $elem ){
-    $rv = ! empty( $elem );
-    return $rv;
-  } );
-  $pathArrCount = count( $pathArr );
-  if( 1 > $pathArrCount ){
-    throw new \Exception( 'wrong path!', 404 );
-  }
-  $upperLevels = implode( '/', array_map( function(){
-    return '..';
-  }, range( 0, $pathArrCount ) ) );
+function getLoaderPath()
+{
+    $scriptName = $_SERVER['SCRIPT_NAME'];
+    $path = dirname($scriptName);
+    $pathArr = array_filter(explode('/', $path), function ($elem) {
+        $rv = ! empty($elem);
+        return $rv;
+    });
+    $pathArrCount = count($pathArr);
+    if (1 > $pathArrCount) {
+        throw new \Exception('wrong path!', 404);
+    }
+    $upperLevels = implode('/', array_map(function () {
+        return '..';
+    }, range(0, $pathArrCount)));
 
-  return __DIR__ . "/$upperLevels/lib/Henson/Henson.php";
+    return __DIR__ . "/$upperLevels/lib/Henson/Henson.php";
 }
 
 $loaderPath = getLoaderPath();
